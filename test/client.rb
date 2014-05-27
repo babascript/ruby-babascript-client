cf8be0c6065e8c166f5ae8920d227001e3ef0b24
TakumiBaba <s09704tb@gmail.com>
2014-05-27 18:40:22 +0900

    ad test files

---
+++ test/client.rb
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+require File.expand_path 'test_helper', File.dirname(__FILE__)
+
+class TestBabaScriptClient < MiniTest::Test
+
+  def test
+    assert_equal nil, nil
+
+  end
+
+end
